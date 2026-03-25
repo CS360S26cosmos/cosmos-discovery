540e63e2e3886be6f6bf09cd8ad8365de322d082
Ammara <143942734+amm4ra@users.noreply.github.com>
2026-03-25 19:52:22 +0500

    added role-based routing (#26)

---
+++ app/src/main/java/com/example/cosmos_discovery/ui/admin/AdminActivity.java
@@ -0,0 +1,15 @@
+package com.example.cosmos_discovery.ui.admin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cosmos_discovery.R;
+
+public class AdminActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
