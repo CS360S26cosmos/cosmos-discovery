e502dd7e3c2487c45dbe29714e545d5723e5fc3f
Elizeh Faisal <27100052@lums.edu.pk>
2026-04-07 19:19:48 +0500

    Add reset password

--- app/src/main/java/com/example/cosmos_discovery/ui/shared/SettingsActivity.java
+++ app/src/main/java/com/example/cosmos_discovery/ui/shared/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.cosmos_discovery.ui.shared;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.cosmos_discovery.R;
@@ -11,5 +12,7 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         findViewById(R.id.btnBack).setOnClickListener(v -> finish());
+        findViewById(R.id.tvResetPassword).setOnClickListener(v ->
+                startActivity(new Intent(this, ResetPasswordActivity.class)));
     }
 }
